0603a50bf0aa7fffd7e76a79d06eaeee76b86b44
dvitamini <boransent@gmail.com>
2011-09-08 21:56:41 +0300

    initial commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,6 @@ class PagesController < ApplicationController
 
   def contact
   end
-
+  def about
+  end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe PagesController do
-
+render_views
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -15,5 +15,11 @@ describe PagesController do
       response.should be_success
     end
   end
-
+describe "GET 'about'" do
+it "should be successful" do
+get 'about'
+response.should be_success
 end
+end
+end
+
